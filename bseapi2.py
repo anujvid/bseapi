193582ab364e41f3e2d207fd80cf9b883e27b8ee
anujvid <anujvid@gmail.com>
2018-12-24 21:46:30 +0530

    Revert "Changes in api"
    This reverts commit 6e1f8061b021b4eac0e76d6a5c35f43c3ed99157.

--- bseapi2.py
+++ bseapi2.py
@@ -119,6 +119,8 @@ def getstockquote(companycode,query):
                 
         return speech
 
+        
+
 def getperformance(companycode,query):
 
         try:
@@ -160,7 +162,7 @@ def getbseindex():
         data = page.text
         data = data.split(',')
         w= 1.00417 
-    	b= -0.144424
+        b= -0.144424
         x = float(data[1])
         newindex = round(((x*w)+b),2)
         speech = data [0] + ": " +data [1] + " Change of " + data [2] + " points and % of " + data[3] + " and my prediction is : " + str(newindex) 
